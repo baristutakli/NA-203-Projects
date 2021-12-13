2524b6ad91b2f9cf17ca362a98f941f7b4148057
baristutakli <baristutakli@hotmail.com>
2021-12-13 11:08:28 +0300

    DataBase Connection, Select, Generic

--- Ders22/CustomProject/Tools.cs
+++ Ders22/CustomProject/Tools.cs
@@ -3,10 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Reflection;
 
 namespace Common
 {
-    public class Tools
+    public static class Tools
     {
+        private static SqlConnection _connection;
+
+        public static SqlConnection Connection
+        {
+            get
+            {
+                if (_connection == null)
+                {
+                    _connection = new SqlConnection(@"Server:(localdb)\MSSQLLocalDB;Database=Northwind;Trusted_Connection=true; ");
+                }
+                return _connection;
+            }
+            set
+            {
+                _connection = value;
+            }
+        }
+
+        /// <summary>
+        /// Where ile et class tipinde olma zorunluluğu belirttik
+        /// aynı şekilde new() ile bu sınıfın nesne türetilebilir bir sınıf olması zorunluluğunu belirttik
+        /// Method binding: this DataTable ile DataTable sınıfına method ekledik.
+        /// </summary>
+        /// <typeparam name="ET"></typeparam>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static List<ET> ToList<ET>(this DataTable dt) where ET : class, new()
+        {
+            Type type = typeof(ET);// Gelen tip ne
+            List<ET> list = new List<ET>(); // gelen tip nesnelerin olduğu bir liste
+            // reflection ile bir sınıfa ait hangi özellikler var öğrenebiliyorum
+            PropertyInfo[] properties = type.GetProperties();// sınıfa ait özellikleri bir diziye attık
+            foreach (DataRow dr in dt.Rows)
+            {
+                ET tip = new ET();
+                foreach (PropertyInfo pi in properties)
+                {
+                    object value = dr[pi.Name];// Sınıf özelliğinin adı ile veri tabanındaki tablodan veri çektik
+                    // Yani datarow[CategoryName] dediğimizde oradaki veriye ulaşmış olacağız.
+                    if (value != null)
+                    {
+                        pi.SetValue(tip, value);
+                    }
+                }
+                list.Add(tip);
+            }
+            return list;
+        }
+
     }
 }
---
+++ Ders22/ORM/ORMBase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace ORM
+{
+    public class ORMBase<ET> : IORM<ET> where ET : class, new()
+    {
+        public bool Delete(ET entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(ET entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ET> Select()
+        {
+            Type type = typeof(ET);
+            string query = "SELECT * FROM ";
+            
+            var attributes = type.GetCustomAttributes(typeof(Table), false);
+            // null değilse ve herhangi bir data varsa
+            if (attributes != null && attributes.Any())
+            {
+                Table tbl = (Table)attributes[0];// 0 dediğimizde TableName'i almış olduk
+                query += tbl.TableName+ ";";
+            }
+            SqlDataAdapter adp = new SqlDataAdapter();
+            adp.SelectCommand = new SqlCommand();
+            adp.SelectCommand.CommandText = query;
+            // Connection static Tools sınıfından geldi.
+            adp.SelectCommand.Connection = Tools.Connection;
+
+            DataTable dt = new DataTable();
+            adp.Fill(dt);// gelen veriyi dt ye doldurduk
+            return dt.ToList<ET>(); 
+            // Burada Tools sınıda tanımlayıp DataTable'a bind 
+            // ettiğimiz metodu DataTable nesnesi aracılığı ile kullandık.
+            
+
+        }
+
+        public bool Update(ET entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
