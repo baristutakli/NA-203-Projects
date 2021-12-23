1087d5b1db320fab57614b5889bef912eb890d3c
baristutakli <baristutakli@hotmail.com>
2021-12-23 20:03:20 +0300

    Func and Events

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -15,6 +15,21 @@ namespace Delegates
             myDelegate += customer.ShowAlert;// ikinci bir işlemi delege ettik
             // -= kullanarak yapılan işlemi ger alabiliriz
             myDelegate();
+
+            Func<int, int, int> add = Topla;// add operasyonu aslında bir delegeye karşılık geliyor
+            Console.WriteLine(add(3,5));
+
+            Func<int> getRandom = delegate ()
+            {
+                Random rdm = new Random();
+                return rdm.Next(1, 100);
+            };
+
+            Func<int> getRandom2 = () => new Random().Next(1, 50);// parametresiz metoda delegasyon yaptık
+        }
+        static int Topla(int a,int b)
+        {
+            return a + b;
         }
     }
     public class CustomerManager {
---
+++ Events/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Product Hdd = new Product(50);
+            Hdd.ProductName = "Hard disk";
+
+            Product ssd = new Product(50);
+            ssd.ProductName = "Ssd";
+            ssd.StockControlEvent += Ssd_StockControlEvent;
+            for (int i = 0; i < 10; i++)
+            {
+                Hdd.Sell(10);
+                ssd.Sell(10);
+
+            }
+        
+        }
+        private static void Ssd_StockControlEvent()
+        {
+            Console.WriteLine("SSd about to finish");
+        }
+    }
+}
